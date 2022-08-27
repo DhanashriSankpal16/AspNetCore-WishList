57d54de1c343c92721085a1ae6b3a2f28d0ba42c
Dhanashri Sankpal <dsankpal675@gmail.com>
2022-08-27 16:08:50 +0000

    Error

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -12,6 +12,9 @@ namespace WishList.Controllers
         {
             return View("Index");
         }
-       
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
     }
 }
