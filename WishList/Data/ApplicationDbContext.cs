82ecd3064d4f5023b99adcba2b4035ecacb70e48
DhanashriSankpal16 <dsankpal675@gmail.com>
2022-12-05 17:58:30 +0530

    Data

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext
+    {
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions options) : base(options)
+            {
+            }
+
+            public DbSet<Models.Item> Items { get; set; }
+        }
+    }
+}
+
